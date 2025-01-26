63f281e8b070f5bbb99dc25ead25a51901cb51e6
neerajk <neerajtheboss10@gmail.com>
2025-01-26 18:26:51 +0530

    inheritance

---
+++ contracts/bc class/inheritance.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract parent{
+
+    string name;
+    uint8 age;
+
+    function setAge(string memory _name, uint8 _age) public {
+        name = _name;
+        age = _age;
+    }
+
+    function getAge() public view returns(uint8){
+        return age;
+    }
+}
+
+contract child is parent{
+    
+    function getName() public view returns(string memory){
+        return name;
+    }
+
+}
